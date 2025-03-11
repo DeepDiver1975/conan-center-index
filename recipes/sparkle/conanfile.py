59104e3a598fb0209bf66c32395cf0062d712c49
Thomas Müller <1005065+DeepDiver1975@users.noreply.github.com>
2025-03-11 22:33:48 +0100

    sparkle: add version 2.7.0

---
+++ recipes/sparkle/conanfile.py
@@ -0,0 +1,28 @@
+from conan import ConanFile
+from conan.tools.apple import XcodeBuild
+from conan.tools.files import get
+
+class sparkleRecipe(ConanFile):
+    name = "sparkle"
+    version = "2.7.0"
+    package_type = "library"
+
+    # Optional metadata
+    license = "<Put the package license here>"
+    author = "<Put your name here> <And your email here>"
+    url = "<Package recipe repository url here, for issues about the package>"
+    description = "<Description of sparkle package here>"
+    topics = ("<Put some tag here>", "<here>", "<and here>")
+
+    # Binary configuration
+    settings = "os", "compiler", "build_type", "arch"
+    options = {"shared": [True, False], "fPIC": [True, False]}
+    default_options = {"shared": False, "fPIC": True}
+
+    def source(self):
+        get(self, "https://github.com/sparkle-project/Sparkle/archive/refs/tags/2.7.0.tar.gz",
+                  strip_root=True)
+
+    def build(self):
+        xcodebuild = XcodeBuild(self)
+        xcodebuild.build("Sparkle.xcodeproj")
